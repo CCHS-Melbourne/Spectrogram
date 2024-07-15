4c9405cd42398c230951abbefe26b9ff4f48adc2
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-07-15 10:49:36 +1000

    Getting I2S init errors:
            #>>> m = mic.Mic()
            #E (37838) i2s_common: i2s_new_channel(867): there should be at least 2 DMA buffers
            #E (37848) i2s_std: i2s_channel_init_std_mode(204): input parameter 'handle' is NULL
            #E (37848) i2s_common: i2s_channel_register_event_callback(316): input parameter 'handle' is NULL
            #E (37858) i2s_common: i2s_channel_enable(1077): input parameter 'handle' is NULL
    
    I suspect that's ESP-IDF from main (HEAD) since they seem to start moving I2S to a separate compenent/driver that might require DMA arguments.
    
    So this means to re-run the builder with ESP-IDF 5.0.2 to rebuild micropython image to a known-stable IDF.

--- src/micropython/async/main.py
+++ src/micropython/async/main.py
@@ -1,11 +1,14 @@
 import asyncio
 from leds import Leds
+from mic import Mic
 from debug import set_global_exception
 
 async def main():
     set_global_exception()
     lit = Leds()
-    await asyncio.create_task(lit.dance())
+    m = Mic()
+    await asyncio.run(m.run())
+    # await asyncio.create_task(lit.dance())
     # await leds.run_forever()  # Non-terminating method
 try:
     asyncio.run(main())
