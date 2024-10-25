94cb769b6ee7f0d6f3a58bf857b5f08892c54f88
brainstorm <brainstorm@nopcode.org>
2024-10-25 22:13:48 +1100

    Try another rearrangement when spawning async tasks: first we spawn the 3 touch buttons and then mic... mic task only seems to work when it's the only task spawned (when no touch tasks are spawned)... and in any case, touch reliability seems to be erratic? See https://github.com/micropython/micropython/issues/13178#issuecomment-2254331069 /cc @projectgus (do let me know if you need more context and/or a better explanation)

--- src/micropython/main.py
+++ src/micropython/main.py
@@ -13,10 +13,10 @@ async def main():
 
     print("Starting main gather...")
     #await asyncio.gather(microphone.start(), touch0.start())#, touch1.start(), touch2.start())
-    await asyncio.create_task(microphone.start())
     await asyncio.create_task(touch0.start())
-    #await asyncio.create_task(touch1.start())
-    #await asyncio.create_task(touch2.start())
+    await asyncio.create_task(touch1.start())
+    await asyncio.create_task(touch2.start())
+    await asyncio.create_task(microphone.start())
 try:
     asyncio.run(main())
 finally:
